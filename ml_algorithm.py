c392f680413035b6d81bf514c95c43bdb9d213ce
PaulinaLopezC <gpaulopez@gmail.com>
2025-08-24 21:57:20 -0600

    Agregar tecnica de aprendizaje ML ml_algorithm.py

---
+++ ml_algorithm.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Módulo 2 Implementación de una técnica de aprendizaje máquina sin el uso de un framework. (Portafolio Implementación)
+# Guadalupe Paulina López Cuevas
+# A01701095
+# 
+# Linear Regression Model + Logaritmic Cost Function
+# ---------------------------------------------------------------------------------------------------------------------
+
