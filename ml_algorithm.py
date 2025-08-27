8096aed1918cb6efbc3cad843dc5deb03c78b636
PaulinaLopezC <gpaulopez@gmail.com>
2025-08-27 14:44:51 -0600

    Agregar descripcion de algunas funciones, mover llamado a removes, outliers al main

--- ml_algorithm.py
+++ ml_algorithm.py
@@ -11,25 +11,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
+# Funcion para cargar el data set desde un archivo para manipular los datos
 def load_data(file_path):
-    # Cargar el data set desde el .csv para manipular los datos
     df = pd.read_csv(file_path, sep=',')
     print("LOADED DATASET\n", df)
     return df
 
+# Funcion para revisar porcentaje de valores nulos en el nuestro dataset
 def null_count(df):
     df_nulos = df.isnull().mean()*100
     print("NULL DATA\n", df_nulos)
     return df_nulos
 
+# Funcion para revisar si existe algun valor duplicado y quitarlo del dataset
 def duplicate_exist(df):
   df_nonduplicate = df.drop_duplicates()
   print("NO MORE DUPLICATED\n", df_nonduplicate)
   return df_nonduplicate
-    
+
+# Funcion para quitar alguna colunma en especifico
+# Objetivo: remover datos del dataset que no sean considerados relevantes para nuestro entrenamiento
 def remove_subject_column(df, column):
     return df.drop(column, axis=1)
 
+# Funcion para deshacernos de los valores que son anomalias(ruido o errores de medicion)
+# Objetivo: Tomar los valores que se encuentren entre el 25% y 75% de los datos originales
+# #para despreciar extremos muy fuera del comportamiento normal
 def outliers_drop(df, columns):
   for i in columns:
         Q1 = df[i].quantile(0.25)
@@ -45,16 +52,16 @@ def clean_dataset(df):
     df.info()
     df.describe()
     df = duplicate_exist(df)
-    df_clean = remove_subject_column(df, 'subject#')
-    df_clean = remove_subject_column(df_clean, 'test_time')
-    columns =  ['Jitter(%)', 'Jitter(Abs)', 'Jitter:PPQ5', 'Jitter:DDP', 'Shimmer', 'Shimmer(dB)', 'Shimmer:APQ3', 'Shimmer:APQ5', 'Shimmer:APQ11', 'Shimmer:DDA', 'NHR', 'HNR', 'RPDE', 'DFA', 'PPE']
-    df_clean = outliers_drop(df_clean, columns)
-    return df_clean
+    return df
 
 
 def main():
     df = load_data('parkinsons_updrs.data')
     df_clean = clean_dataset(df)
+    df_clean = remove_subject_column(df, 'subject#')
+    df_clean = remove_subject_column(df_clean, 'test_time')
+    columns =  ['Jitter(%)', 'Jitter(Abs)', 'Jitter:PPQ5', 'Jitter:DDP', 'Shimmer', 'Shimmer(dB)', 'Shimmer:APQ3', 'Shimmer:APQ5', 'Shimmer:APQ11', 'Shimmer:DDA', 'NHR', 'HNR', 'RPDE', 'DFA', 'PPE']
+    df_clean = outliers_drop(df_clean, columns)
     df_x = df_clean[['age', 'sex', 'Jitter(%)', 'Jitter(Abs)', 'Jitter:PPQ5', 'Jitter:DDP', 'Shimmer', 'Shimmer(dB)', 'Shimmer:APQ3', 'Shimmer:APQ5', 'Shimmer:APQ11', 'Shimmer:DDA', 'NHR', 'HNR', 'RPDE', 'DFA', 'PPE']]
     df_y1 = df_clean[['total_UPDRS']]
     df_y2 = df_clean[['motor_UPDRS']]
